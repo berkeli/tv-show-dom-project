8618d236a15ed6d16f383cbb076d72d2fbf70d1d
Berkeli H <berkelih@gmail.com>
2022-02-19 17:43:11 +0000

    Installed eslint and general setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ignorePatterns: ['episodes.js'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
