89b75af2c764fde8a3868324953f3807dbaf56ec
Berkeli H <berkelih@gmail.com>
2022-02-19 18:37:16 +0000

    Level 100 completed

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+/* eslint-disable no-undef */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
+let allEpisodes;
+const rootElem = document.getElementById('root');
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+const createEpisodeEl = ({
+  name, season, number, image, summary,
+}) => {
+  // Create a box
+  const episodeEl = document.createElement('li');
+  episodeEl.classList.add('episode');
+
+  // Create Episode Title
+  const titleEl = document.createElement('h3');
+  season = season.toString().padStart(2, '0');
+  episode = number.toString().padStart(2, '0');
+  titleEl.innerText = `${name} - S${season}E${episode}`;
+
+  // Create image element
+  const imgEl = document.createElement('img');
+  imgEl.setAttribute('src', image.medium);
+
+  // Create description
+  const descriptionEl = document.createElement('span');
+  descriptionEl.innerHTML = summary;
+
+  // Add Everything together
+  episodeEl.appendChild(titleEl);
+  episodeEl.appendChild(imgEl);
+  episodeEl.appendChild(descriptionEl);
+
+  return episodeEl;
+};
+
+const renderEpisodes = () => {
+  // Create container
+  const listEl = document.createElement('ul');
+  listEl.classList.add('episodes-container');
+
+  // Add episodes
+  allEpisodes.forEach((episode) => listEl.appendChild(createEpisodeEl(episode)));
+
+  // Render the container with children
+  rootElem.appendChild(listEl);
+};
+
+const setup = () => {
+  allEpisodes = getAllEpisodes();
+  renderEpisodes();
+};
 
 window.onload = setup;
